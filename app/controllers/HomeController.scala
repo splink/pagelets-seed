b26bae9ac84594e781f5a12523dbc3ab50a7a757
Max Kugland <maxkugland@gmail.com>
2016-10-22 21:27:09 +0200

    add a 'sticky' footer and restructure the template composition accordingly

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -23,13 +23,15 @@ class HomeController @Inject()(c: PageletController)(implicit m: Materializer, e
       Tree('header, Seq(
         Leaf('navigation, navigation _).withFallback(navigationFallback _)
       )),
-      Leaf('carousel, carousel _),
-      Tree('texts, Seq(
-        Leaf('textA, text("A") _),
-        Leaf('textB, text("B") _)
-      ), results => combine(results)(views.html.pagelets.sixsix.apply)),
+      Tree('content, Seq(
+        Leaf('carousel, carousel _),
+        Tree('texts, Seq(
+          Leaf('textA, text("A") _),
+          Leaf('textB, text("B") _)
+        ), results => combine(results)(views.html.pagelets.sixsix.apply))
+      )),
       Leaf('footer, footer _)
-    ))
+    ), results => combine(results)(views.html.sections.apply))
     tree/*.replace('brick2, Leaf('yo, yo _))*/
   }
 
@@ -38,7 +40,7 @@ class HomeController @Inject()(c: PageletController)(implicit m: Materializer, e
 
   def resourceFor(fingerprint: String) = ResourceAction(fingerprint)
 
-  def index = PageAction(errorTemplate)("Index", tree, Arg("s", "Hello!")) { (request, page) =>
+  def index = PageAction(errorTemplate)("Index", tree) { (request, page) =>
     log.info(visualize(tree(request)))
     mainTemplate(page)
   }
@@ -57,7 +59,7 @@ class HomeController @Inject()(c: PageletController)(implicit m: Materializer, e
   def carousel = Action { implicit request =>
     Ok(views.html.pagelets.carousel()).
       withJavascript(
-        Javascript("lib/bootstrap/js/transition.js"),
+        Javascript("lib/bootstrap/js/transition.min.js"),
         Javascript("lib/bootstrap/js/carousel.js"))
   }
 
@@ -65,8 +67,10 @@ class HomeController @Inject()(c: PageletController)(implicit m: Materializer, e
     Ok(views.html.pagelets.text(typ))
   }
 
-  def footer(s: String) = Action { implicit request =>
-    Ok(views.html.pagelets.footer())
+  def footer = Action { implicit request =>
+    Ok(views.html.pagelets.footer()).withCss(
+      Css("stylesheets/footer.min.css")
+    )
   }
 
   def navigationFallback = Action {
